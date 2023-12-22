23ce87a76f7b832e0087fd92f880ab9f6c1c8290
Fayzillo Khamraev <absolemgen@gmail.com>
2023-12-22 11:06:42 +0100

    hm 3 v2

--- js/task-3.js
+++ js/task-3.js
@@ -1,4 +1,3 @@
-debugger;
 function filterArray(numbers, value) {
   let biggest = [];
   for (let i = 0; i < numbers.length; i++) {
@@ -8,8 +7,8 @@ function filterArray(numbers, value) {
   }
   return biggest;
 }
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+filterArray([1, 2, 3, 4, 5], 3); // [4, 5]
+filterArray([1, 2, 3, 4, 5], 4); // [5]
+filterArray([1, 2, 3, 4, 5], 5); // []
+filterArray([12, 24, 8, 41, 76], 38); // [41, 76]
+filterArray([12, 24, 8, 41, 76], 20); // [24, 41, 76]
